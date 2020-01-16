62642073a6b152675b983d3a8b0b02246cfb5f98
kamath-abhijith <kamath.abhijith@gmail.com>
2020-01-16 17:51:51 +0530

    omp corrections

--- MATLAB/omp/mat_pursuit.m
+++ MATLAB/omp/mat_pursuit.m
@@ -8,13 +8,15 @@ f = load_dict_f.f;
 psi = load_dict_f.Psi;
 
 %% Matching Pursuit
-[fmp_est, residual_mp, sparse_mpcode] = matching_pursuit(f,psi,32);
+sparsemp_code = matching_pursuit(f,psi,32);
+fmp_est = psi*sparsemp_code;
 
 %% Orthogonal Matching Pursuit
-[fomp_est, residual_omp, sparse_ompcode] = orthogonal_pursuit(f,psi,32);    
+sparseomp_code = orthogonal_pursuit(f,psi,32);
+fomp_est = psi*sparseomp_code;
 
 %% Plots
-figure, subplot(2,1,1)
+figure, subplot(2,2,1)
 stem(fmp_est,'-r',"LineWidth",2)
 hold on, grid on
 stem(f,'--b',"LineWidth",1)
@@ -22,10 +24,22 @@ legend('Ground Truth','Reconstruction')
 xlabel('n'), ylabel('f(n)')
 title('Reconstruction using Matching Pursuit')
 
-subplot(2,1,2)
+subplot(2,2,2)
+stem(sparsemp_code,'-b',"LineWidth",2)
+grid on
+xlabel('k')
+title('Sparse Code found using Matching Pursuit')
+
+subplot(2,2,3)
 stem(fomp_est,'-r',"LineWidth",2)
 hold on, grid on
 stem(f,'--b',"LineWidth",1)
 legend('Ground Truth','Reconstruction')
 xlabel('n'), ylabel('f(n)')
 title('Reconstruction using Orthogonal Matching Pursuit')
+
+subplot(2,2,4)
+stem(sparseomp_code,'-b',"LineWidth",2)
+grid on
+xlabel('k')
+title('Sparse Code found using Orthogonal Matching Pursuit')
