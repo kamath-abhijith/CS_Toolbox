53e1e39b15ee979e14a0505cdef8ae8e48254e6e
kamath-abhijith <kamath.abhijith@gmail.com>
2020-01-22 17:33:55 +0530

    OMP

--- python/omp/main.py
+++ python/omp/main.py
@@ -3,24 +3,57 @@ from scipy.io import loadmat
 
 from matplotlib import pyplot as plt
 from matplotlib import style
+from matplotlib import rcParams
 
 from matchingPursuit import *
 
 ## Load signal and dictionary
 load_dict_f = loadmat('hw1problem3.mat')
 
-Psi = load_dict_f['Psi']
-f = load_dict_f['f']
+A = load_dict_f['Psi']
+b = load_dict_f['f']
 
-sparse_code = mat_pursuit(Psi,f,32)
-f_rec = Psi.dot(sparse_code)
+x_mp = greedy_mp(A,b,32)
+b_rec_mp = A.dot(x_mp)
+
+x_omp = greedy_omp(A,b,32)
+b_rec_omp = A.dot(x_omp)
+
+## Error metrics
+eb_mp = b-b_rec_mp
+eb_mp_norm = np.linalg.norm(eb_mp)
+eb_mp_norm_db = 10*np.log10(eb_mp_norm)
+print("l2 norm of the error using MP: ",eb_mp_norm_db,"dB")
+
+eb_omp = b-b_rec_omp
+eb_omp_norm = np.linalg.norm(eb_omp)
+eb_omp_norm_db = 10*np.log10(eb_omp_norm)
+print("l2 norm of the error using OMP: ",eb_omp_norm_db,"dB")
 
 ## Plots
 style.use('ggplot')
-style.use('dark_background')
+# style.use('dark_background')
 
-fig, plts = plt.subplots(2)
-plts[0].plot(f,color='green')
-plts[0].plot(f_rec,'--',color='red')
-plts[1].stem(sparse_code,'-b')
+rcParams['text.usetex'] = True
+
+fig, plts = plt.subplots(2,2)
+plts[0][0].plot(b,color='green',label='True')
+plts[0][0].plot(b_rec_mp,'--',color='red',label='Estimate')
+plts[0][0].set_xlabel(r"m")
+plts[0][0].set_ylabel(r"b")
+plts[0][0].set_title(r"Matching Pursuit")
+plts[0][0].legend()
+plts[1][0].stem(x_mp,'-b')
+plts[1][0].set_xlabel(r"N")
+plts[1][0].set_ylabel(r"x")
+
+plts[0][1].plot(b,color='green',label='True')
+plts[0][1].plot(b_rec_omp,'--',color='red',label='Estimate')
+plts[0][1].set_xlabel(r"m")
+plts[0][1].set_ylabel(r"b")
+plts[0][1].set_title(r"Orthogonal Matching Pursuit")
+plts[0][1].legend()
+plts[1][1].stem(x_omp,'-b')
+plts[1][1].set_xlabel(r"N")
+plts[1][1].set_ylabel(r"x")
 plt.show()
--- python/omp/matchingPursuit.py
+++ python/omp/matchingPursuit.py
@@ -1,17 +1,115 @@
 import numpy as np
 
-def mat_pursuit(A,b,iter_lim):
+def greedy_mp(A,b,*argv):
+    '''
+    Sparse recovery using matching pursuit
+
+    INPUT:  Measurement matrix, A
+            Measurements, b
+            Optional: Number of iterations
+                      Error tolerance
+    OUTPUT: Estimated sparse vector
+
+    Author: Abijith J. Kamath
+    kamath-abhijith.github.io
+    '''
+
+    ## Read optional arguments
+    nargin = len(argv)
+    if (nargin >= 1):
+        iter_lim = argv[0]
+    else:
+        iter_lim = len(b)
+
+    if (nargin >= 2):
+        tol = argv[1]
+    else:
+        tol = 1e-6
+
+    ## Initializations
     m,n = np.shape(A)
     bmp = np.zeros((m,1))
-    sparse_code = np.zeros((n,1))
+    x = np.zeros((n,1))
     res_b = b[:,0]
-    
+
+    ## Iterations
     for i in range(iter_lim):
+
+        # Projection step
         weights = A.transpose().dot(res_b)
         idx = np.argmax(np.abs(weights))
-        sparse_code[idx] = sparse_code[idx] + weights[idx]
+
+        # Update step
+        x[idx] = x[idx] + weights[idx]
         update = weights[idx]*A[:,idx]
         bmp = bmp + update
+
+        # Residue corrections
         res_b = res_b - update
 
-    return sparse_code
+        # Stopping criteria
+        e_rec = np.linalg.norm(b-A.dot(x))**2
+        if (e_rec < tol):
+            break
+
+    return x
+
+def greedy_omp(A,b,*argv):
+    '''
+    Sparse recovery using orthogonal matching pursuit
+
+    INPUT:  Measurement matrix, A
+            Measurements, b
+            Optional: Number of iterations
+                      Error tolerance
+    OUTPUT: Estimated sparse vector
+
+    Author: Abijith J. Kamath
+    kamath-abhijith.github.io
+    '''
+
+    ## Read optional arguments
+    nargin = len(argv)
+    if (nargin >= 1):
+        iter_lim = argv[0]
+    else:
+        iter_lim = len(b)
+
+    if (nargin >= 2):
+        tol = argv[1]
+    else:
+        tol = 1e-6
+
+
+    ## Initialization
+    m,n = np.shape(A)
+    bomp = np.zeros((m,1))
+    x = np.zeros((n,1))
+    supp = []
+    res_b = b[:,0]
+
+    ## Iterations 
+    for i in range(iter_lim):
+
+        # Projection step
+        proj = A.transpose().dot(res_b)
+        idx = np.argmax(np.abs(proj))
+
+        # Support update
+        supp = np.union1d(supp,[idx])
+        supp = np.sort(supp)
+
+        # Orthogonal projection step
+        As = A[:,supp.astype(int)]
+        xs = np.linalg.pinv(As).dot(b)
+        x[supp.astype(int)] = xs
+
+        # Residue correction
+        res_b = b[:,0] - A.dot(x)[:,0]
+
+        # Stopping criteria
+        e_rec = np.linalg.norm(b-A.dot(x))**2
+        if (e_rec < tol):
+            break
+
+    return x
